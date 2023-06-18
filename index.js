496bd465d1b273e2a8e541da138981305d831327
thebadboy78 <kirz@list.ru>
2023-06-18 21:39:12 +0300

    server
    развернули сервак

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let a = 5;
-let b = 10;
+import express from 'express';
 
+const app = express();
 
+app.get('/', (req, resp) => {
+    resp.send('Hello World!');
+});
 
-console.log(a + b, 'Hello');
+app.listen(4444, (err) => {
+    if(err){
+        return console.log(err);
+    }
+
+    console.log('Server OK');
+});
