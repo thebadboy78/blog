150f28b65c831f646bd02205e9fbf9cb64608dab
thebadboy78 <kirz@list.ru>
2023-06-21 20:31:43 +0300

    second
    second

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, resp) => {
     resp.send('Hello World!');
+});
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+    res.json({
+        success:true,
+    });
 });
 
 app.listen(4444, (err) => {
