4b5523af34c8740dc781c500e2b4ddf53a73083c
thebadboy78 <kirz@list.ru>
2023-06-25 13:34:55 +0300

    4

--- index.js
+++ index.js
@@ -11,25 +11,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, resp) => {
-    resp.send('Hello World!');
-});
+app.post('/auth/register', (req, res) => {
 
-app.post('/auth/login', (req, res) => {
-    console.log(req.body);
-
-const token = jwt.sign(
-    {
-        email: req.body.email,
-        fullName: 'Вася Пупкин',
-    },
-    'secret23',
-);
-
-    res.json({
-        success:true,
-        token,
-    });
 });
 
 app.listen(4444, (err) => {
@@ -39,3 +22,27 @@ app.listen(4444, (err) => {
 
     console.log('Server OK');
 });
+
+
+
+
+// app.post('/auth/login', (req, res) => {
+//     console.log(req.body);
+
+// const token = jwt.sign(
+//     {
+//         email: req.body.email,
+//         fullName: 'Вася Пупкин',
+//     },
+//     'secret23',
+// );
+
+//     res.json({
+//         success:true,
+//         token,
+//     });
+// });
+
+// app.get('/', (req, resp) => {
+//     resp.send('Hello World!');
+// });
